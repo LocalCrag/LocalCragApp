08ca1607a162ed1f29f6757af4c7da991c4a47cb
Felix Engelmann <felix.engelmann@labnode.eu>
2023-05-20 21:59:52 +0200

    Downgrade Python Version, Add htaccess

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiHost: 'https://localcrag.engelmann.cloud',
+  regionId: 'd2c864b4-ca80-4d01-a8bf-41521182b5d4',
+  language: 'de'
 };
