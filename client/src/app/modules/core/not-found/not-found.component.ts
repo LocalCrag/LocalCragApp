a923ee6e52f2e6eac76877dc9b4983ef457eec92
Felix Engelmann <felix.engelmann@labnode.eu>
2024-02-11 10:16:41 +0100

    Add nice 404 error page

--- client/src/app/modules/core/not-found/not-found.component.ts
+++ client/src/app/modules/core/not-found/not-found.component.ts
@@ -1,9 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {TranslocoService} from '@ngneat/transloco';
 import {marker} from '@ngneat/transloco-keys-manager/marker';
 import {environment} from '../../../../environments/environment';
+import {LoadingState} from '../../../enums/loading-state';
+import {Router} from '@angular/router';
 
+/**
+ * A 404 error page.
+ */
 @Component({
   selector: 'lc-not-found',
   templateUrl: './not-found.component.html',
@@ -11,11 +16,16 @@ import {environment} from '../../../../environments/environment';
 })
 export class NotFoundComponent implements OnInit{
 
+  @HostBinding('class.auth-view') authView: boolean = true;
+
+  public url: string;
+
   constructor(private title: Title,
               private translocoService: TranslocoService) {
   }
 
   ngOnInit() {
+    this.url = window.location.href;
     this.title.setTitle(`${this.translocoService.translate(marker('notFoundPageBrowserTitle'))} - ${environment.instanceName}`)
   }
 
