471755ea4bab628e7b6132874383b6b2b15fb378
Felix Engelmann <felix.engelmann@labnode.eu>
2024-02-25 22:43:24 +0100

    Update slug blocklists

--- server/src/models/sector.py
+++ server/src/models/sector.py
@@ -14,7 +14,7 @@ class Sector(HasSlug, BaseEntity):
     """
     __tablename__ = 'sectors'
 
-    slug_blocklist = ['edit', 'create-sector', 'sectors', 'gallery', 'ascents']
+    slug_blocklist = ['edit', 'create-sector', 'sectors', 'gallery', 'ascents', 'rules']
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text, nullable=True)
     short_description = db.Column(db.Text, nullable=True)
