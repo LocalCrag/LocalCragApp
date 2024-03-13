7c147f31cc9f6bf24f1eca0bd86b4e97f342d145
Felix Engelmann <felix.engelmann@labnode.eu>
2024-03-13 12:29:18 +0100

    Add cypress test for a boulder lookup workflow

--- client/cypress/e2e/boulder-lookup-workflow.cy.ts
+++ client/cypress/e2e/boulder-lookup-workflow.cy.ts
@@ -1,7 +1,12 @@
 describe('Boulder lookup workflow', () => {
   it('navigates step by step to a boulder detail page', () => {
     cy.visit('localhost:4200')
-    // cy.get('[data-cy="navbar-login"]').click()
-    // TODO add workflow
+    cy.get('[data-cy="main-menu"] a').eq(2).click()
+    cy.get('[data-cy="crag-list-item"]').eq(0).click()
+    cy.get('[data-cy="sector-list-item"]').eq(0).click()
+    cy.get('[data-cy="area-list-item"]').eq(0).click()
+    cy.get('[data-cy="area-tabs-menu"] a').eq(1).click()
+    cy.get('[data-cy="line-list-item"]').eq(0).click()
+    cy.get('.p-card-title').contains('Super-Spreader 8A')
   })
 })
