679869777f1587626b54bb2968f28c535ce5732d
Felix Engelmann <felix.engelmann@labnode.eu>
2024-04-16 20:37:30 +0200

    Finish server tests

--- server/tests/test_ascent_resources.py
+++ server/tests/test_ascent_resources.py
@@ -1,5 +1,9 @@
 import json
 
+from sqlalchemy import text
+
+from app import app
+from extensions import db
 from tests.utils.user_test_util import get_login_headers
 
 
@@ -64,6 +68,43 @@ def test_successful_create_ascent(client):
     assert rv.status_code == 200
     res = json.loads(rv.data)
     assert res['ascentCount'] == 2
+
+
+def test_successful_update_ascent(client):
+    access_headers, refresh_headers = get_login_headers(client)
+    ascent_data = {
+        "flash": True,
+        "fa": False,
+        "soft": True,
+        "hard": False,
+        "withKneepad": True,
+        "rating": 2,
+        "comment": "Hahahahaha",
+        "year": None,
+        "gradeScale": "FB",
+        "gradeName": "6A",
+        "line": "9d64b102-95cd-4123-a2d1-4bb1f7c77ba0",
+        "date": "2024-04-13"
+    }
+
+    rv = client.put('/api/ascents/55e840b9-2036-4725-a408-67d977adece5', headers=access_headers, json=ascent_data)
+    assert rv.status_code == 201
+    res = json.loads(rv.data)
+    assert res['flash'] == True
+    assert res['fa'] == False
+    assert res['soft'] == True
+    assert res['hard'] == False
+    assert res['withKneepad'] == True
+    assert res['rating'] == 2
+    assert res['comment'] == "Hahahahaha"
+    assert res['year'] == None
+    assert res['gradeScale'] == "FB"
+    assert res['gradeName'] == "6A"
+    assert res['date'] == "2024-04-13"
+    assert res['line']['slug'] == "super-spreader"
+    assert res['area']['slug'] == "dritter-block-von-links"
+    assert res['sector']['slug'] == "schattental"
+    assert res['crag']['slug'] == "brione"
 
 
 def test_log_twice(client):
@@ -139,3 +180,52 @@ def test_successful_get_ascents(client):
     assert rv.status_code == 200
     res = json.loads(rv.data)
     assert len(res) == 1
+
+
+def test_successful_get_ticks(client):
+    rv = client.get(
+        '/api/ticks?user_id=1543885f-e9ef-48c5-a396-6c898fb42409&crag_id=aabc4539-c02f-4a03-8db3-ea0916e59884')
+    assert rv.status_code == 200
+    res = json.loads(rv.data)
+    assert len(res) == 1
+    assert res[0] == "1c39fd1f-6341-4161-a83f-e5de0f861c48"
+
+    rv = client.get(
+        '/api/ticks?user_id=2543885f-e9ef-48c5-a396-6c898fb42409&crag_id=aabc4539-c02f-4a03-8db3-ea0916e59884')
+    assert rv.status_code == 200
+    res = json.loads(rv.data)
+    assert len(res) == 0
+
+
+def test_successful_delete_ascent(client):
+    access_headers, refresh_headers = get_login_headers(client)
+
+    rv = client.delete('/api/ascents/55e840b9-2036-4725-a408-67d977adece5', headers=access_headers)
+    assert rv.status_code == 204
+
+    # Line must be still there
+    rv = client.get('/api/lines/super-spreader')
+    assert rv.status_code == 200
+
+
+def test_delete_user_deletes_tick_but_not_line(client):
+    # Change ascent owner as you cannot delete own user
+    with app.app_context():
+        with db.engine.begin() as conn:
+            conn.execute(text(
+                "update ascents set created_by_id = '2543885f-e9ef-48c5-a396-6c898fb42409' where created_by_id = '1543885f-e9ef-48c5-a396-6c898fb42409';"))
+
+    access_headers, refresh_headers = get_login_headers(client)
+
+    rv = client.delete('/api/users/2543885f-e9ef-48c5-a396-6c898fb42409', headers=access_headers, json=None)
+    assert rv.status_code == 204
+
+    # Tick is gone
+    rv = client.get('/api/ascents')
+    assert rv.status_code == 200
+    res = json.loads(rv.data)
+    assert len(res) == 0
+
+    # Line must be still there
+    rv = client.get('/api/lines/super-spreader')
+    assert rv.status_code == 200
