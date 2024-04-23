05797ac0fdbffb85f48374cfa7dc2504df087777
Felix Engelmann <felix.engelmann@labnode.eu>
2024-04-23 17:33:56 +0200

    Fix scheduler (#282)
    * Fix scheduler issues
    
    * Fix scheduler issues

--- server/src/schedulers.py
+++ server/src/schedulers.py
@@ -5,6 +5,7 @@ from util.scripts.build_rankings import build_rankings
 
 
 def start_schedulers():
-    if not scheduler.state == 0:
+    if not scheduler.running:
+        build_rankings()
         scheduler.add_job(func=build_rankings, trigger="interval", seconds=60 * 15, id='build_rankings')
         scheduler.start()
