4952b3d2bf1a21e9cff51df1d4198dc698781bc6
Felix Engelmann <felix.engelmann@labnode.eu>
2024-04-23 17:48:11 +0200

    Add debug logs (#283)

--- server/src/schedulers.py
+++ server/src/schedulers.py
@@ -6,6 +6,7 @@ from util.scripts.build_rankings import build_rankings
 
 def start_schedulers():
     if not scheduler.running:
+        print('pre')
         build_rankings()
         scheduler.add_job(func=build_rankings, trigger="interval", seconds=60 * 15, id='build_rankings')
         scheduler.start()
