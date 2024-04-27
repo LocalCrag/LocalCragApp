b779ea38d251b8dde4a42f23957811a34f933826
Felix Engelmann <felix.engelmann@labnode.eu>
2024-04-27 15:40:52 +0200

    Add os env scheduler lock (#303)

--- server/src/schedulers.py
+++ server/src/schedulers.py
@@ -7,7 +7,9 @@ from util.scripts.build_rankings import build_rankings
 
 
 def start_schedulers(app):
-    if not scheduler.running and app.config['ENABLE_SCHEDULERS']:
-        print('Starting scheduler...')
-        scheduler.add_job(func=build_rankings, trigger="interval", seconds=60 * 15, id='build_rankings')
-        scheduler.start()
+    if os.environ.get("scheduler_lock") is None:
+        if not scheduler.running and app.config['ENABLE_SCHEDULERS']:
+            print('Starting scheduler...')
+            scheduler.add_job(func=build_rankings, trigger="interval", seconds=60 * 15, id='build_rankings')
+            scheduler.start()
+            os.environ["scheduler_lock"] = "1"
