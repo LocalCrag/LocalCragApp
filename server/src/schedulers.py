12d847fd052a45c2a806bb35969daf5e95c2f6aa
Felix Engelmann <felix.engelmann@labnode.eu>
2024-04-27 15:25:58 +0200

    Fix ranking (#302)

--- server/src/schedulers.py
+++ server/src/schedulers.py
@@ -7,7 +7,6 @@ from util.scripts.build_rankings import build_rankings
 
 
 def start_schedulers(app):
-    build_rankings()
     if not scheduler.running and app.config['ENABLE_SCHEDULERS']:
         print('Starting scheduler...')
         scheduler.add_job(func=build_rankings, trigger="interval", seconds=60 * 15, id='build_rankings')
