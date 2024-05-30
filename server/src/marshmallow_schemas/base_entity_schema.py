8da6660e150e4f7f8968fb311fa14672b4a7b242
Felix Engelmann <felix.engelmann@labnode.eu>
2024-05-30 16:15:36 +0200

    Use UserMinSchema for user serialization in created by fields

--- server/src/marshmallow_schemas/base_entity_schema.py
+++ server/src/marshmallow_schemas/base_entity_schema.py
@@ -8,4 +8,4 @@ class BaseEntitySchema(ma.SQLAlchemySchema):
     id = fields.String()
     timeCreated = fields.DateTime(attribute="time_created")
     timeUpdated = fields.DateTime(attribute="time_updated")
-    createdBy = ma.Nested('UserSchema', attribute='created_by', exclude=('createdBy', ))
+    createdBy = ma.Nested('UserMinSchema', attribute='created_by')
