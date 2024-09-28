17492c7e85dc2addfb7a5de2efc31b55de17d606
Felix Engelmann <felix.engelmann@labnode.eu>
2024-09-28 18:13:57 +0200

    Fix menu overflow issue

--- client/src/app/modules/shared/components/header-menu/header-menu.service.ts
+++ client/src/app/modules/shared/components/header-menu/header-menu.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import {ChangeDetectorRef, Injectable} from '@angular/core';
 import {ProcessedMenuItem} from './processed-menu-item';
 import {HeaderMenuComponent} from './header-menu.component';
 
@@ -8,10 +8,18 @@ export class HeaderMenuService {
   private activeItem: ProcessedMenuItem;
   private headerMenu: HeaderMenuComponent;
 
-  public setActive(item: ProcessedMenuItem) {
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+  public setActive(item: ProcessedMenuItem, itemElement: HTMLElement = null) {
     this.deactivateCurrent();
     this.activateItem(item);
     this.activeItem = item;
+    // Leaf menu items won't have a sub menu element. They will just be set active.
+    if (itemElement) {
+      this.cdr.detectChanges(); // This is necessary to make sure the menu is rendered before we try to position it.
+      this.fixItemOverflow(itemElement)
+    }
   }
 
   public toggleActive(item: ProcessedMenuItem) {
@@ -51,6 +59,37 @@ export class HeaderMenuService {
 
   public collapseMobileMenu() {
     this.headerMenu.mobileExpanded = false;
+  }
+
+  /**
+   * Detects and fixes menu overflow by three different measures:
+   * 1: If the menu overflows at the bottom, and we have enough space, we move it up.
+   * 2: If it is too high, we make it scrollable and move it to the top of the window.
+   * 3: If it overflows at the right, we make it appear at the left of it's parent instead of the right.
+   */
+  fixItemOverflow(itemElement: HTMLElement) {
+    const itemElementRect = itemElement.getBoundingClientRect();
+    const overflowBottomAmount = itemElementRect.bottom - window.innerHeight;
+    const overflowRightAmount = itemElementRect.right - window.innerWidth;
+    const itemElementHeight = itemElement.offsetHeight;
+
+    // Case 1
+    if (overflowBottomAmount > 0 && !(itemElementHeight > window.innerHeight)) {
+      itemElement.style.setProperty('top', `-${overflowBottomAmount}px`, 'important');
+    }
+
+    // Case 2
+    if (itemElementHeight > window.innerHeight) {
+      itemElement.style.setProperty('top', `-${itemElementRect.top}px`, 'important');
+      itemElement.style.setProperty('height', `${window.innerHeight}px`, 'important');
+      itemElement.style.setProperty('overflow-y', 'auto', 'important');
+    }
+
+    // Case 3
+    if (overflowRightAmount > 0) {
+      itemElement.style.setProperty('left', `-${itemElementRect.width }px`, 'important');
+    }
+
   }
 
 }
