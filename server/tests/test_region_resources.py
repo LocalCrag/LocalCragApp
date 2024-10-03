08f22aa81fd8826281e75a3b6847a6af6b6df7ac
Ben Swierzy <swierzy@uni-bonn.de>
2024-10-03 12:55:38 +0200

    Reworked test_region_resources as sample

--- server/tests/test_region_resources.py
+++ server/tests/test_region_resources.py
@@ -1,12 +1,7 @@
-import json
-
-from tests.utils.user_test_util import get_login_headers
-
-
 def test_successful_get_region(client):
     rv = client.get('/api/region')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert res['id'] == "d2c864b4-ca80-4d01-a8bf-41521182b5d4"
     assert res['name'] == "Tessin"
     assert res['description'] == "Tolle Region"
@@ -14,17 +9,16 @@ def test_successful_get_region(client):
     assert res['ascentCount'] == 1
 
 
-def test_successful_edit_region(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_successful_edit_region(client, admin_token):
     crag_data = {
         "description": "Fodere et scandere. 2",
         "rules": "test rules",
         "name": "Nahetal"
     }
 
-    rv = client.put('/api/region', headers=access_headers, json=crag_data)
+    rv = client.put('/api/region', headers={"Authorization": f"Bearer {admin_token}"}, json=crag_data)
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert res['id'] == "d2c864b4-ca80-4d01-a8bf-41521182b5d4"
     assert res['name'] == "Nahetal"
     assert res['description'] == "Fodere et scandere. 2"
@@ -35,7 +29,7 @@ def test_successful_edit_region(client):
 def test_successful_get_region_grades(client):
     rv = client.get('/api/region/grades')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res) == 2
     assert res[1]['gradeName'] == "1"
     assert res[1]['gradeScale'] == "FB"
