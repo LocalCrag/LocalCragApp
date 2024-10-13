9704587fbc5b45168aff76b5001c07406dd1bcef
Ben Swierzy <swierzy@uni-bonn.de>
2024-10-13 09:47:24 +0200

    Migrated ascent_resources

--- server/tests/test_ascent_resources.py
+++ server/tests/test_ascent_resources.py
@@ -1,15 +1,11 @@
-import json
-
-from pytest_mock import mocker
-from sqlalchemy import text
-
-from app import app
 from extensions import db
-from tests.utils.user_test_util import get_login_headers
+from models.ascent import Ascent
+from models.crag import Crag
+from models.line import Line
+from models.user import User
 
 
-def test_successful_create_ascent(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_successful_create_ascent(client, member_token):
     ascent_data = {
         "flash": True,
         "fa": False,
@@ -21,13 +17,13 @@ def test_successful_create_ascent(client):
         "year": None,
         "gradeScale": "FB",
         "gradeName": "6A",
-        "line": "9d64b102-95cd-4123-a2d1-4bb1f7c77ba0",
+        "line": str(Line.get_id_by_slug("treppe")),
         "date": "2024-04-13"
     }
 
-    rv = client.post('/api/ascents', headers=access_headers, json=ascent_data)
+    rv = client.post('/api/ascents', token=member_token, json=ascent_data)
     assert rv.status_code == 201
-    res = json.loads(rv.data)
+    res = rv.json
     assert res['flash'] == True
     assert res['fa'] == False
     assert res['soft'] == True
@@ -47,32 +43,33 @@ def test_successful_create_ascent(client):
     # Check updated ascent counts
     rv = client.get('/api/lines/treppe')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert res['ascentCount'] == 1
 
     rv = client.get('/api/areas/dritter-block-von-links')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert res['ascentCount'] == 2
 
     rv = client.get('/api/sectors/schattental')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert res['ascentCount'] == 2
 
     rv = client.get('/api/crags/brione')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert res['ascentCount'] == 2
 
     rv = client.get('/api/region')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert res['ascentCount'] == 2
 
 
-def test_successful_update_ascent(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_successful_update_ascent(client, admin_token):
+    ascent = Ascent.query.first()
+
     ascent_data = {
         "flash": True,
         "fa": False,
@@ -84,13 +81,13 @@ def test_successful_update_ascent(client):
         "year": None,
         "gradeScale": "FB",
         "gradeName": "6A",
-        "line": "9d64b102-95cd-4123-a2d1-4bb1f7c77ba0",
+        "line": str(Line.get_id_by_slug("super-spreader")),
         "date": "2024-04-13"
     }
 
-    rv = client.put('/api/ascents/55e840b9-2036-4725-a408-67d977adece5', headers=access_headers, json=ascent_data)
+    rv = client.put(f'/api/ascents/{ascent.id}', token=admin_token, json=ascent_data)
     assert rv.status_code == 201
-    res = json.loads(rv.data)
+    res = rv.json
     assert res['flash'] == True
     assert res['fa'] == False
     assert res['soft'] == True
@@ -108,8 +105,7 @@ def test_successful_update_ascent(client):
     assert res['crag']['slug'] == "brione"
 
 
-def test_log_twice(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_log_twice(client, admin_token):
     ascent_data = {
         "flash": True,
         "fa": False,
@@ -121,16 +117,15 @@ def test_log_twice(client):
         "year": None,
         "gradeScale": "FB",
         "gradeName": "6A",
-        "line": "1c39fd1f-6341-4161-a83f-e5de0f861c48",
+        "line": str(Line.get_id_by_slug("super-spreader")),
         "date": "2024-04-13"
     }
 
-    rv = client.post('/api/ascents', headers=access_headers, json=ascent_data)
+    rv = client.post('/api/ascents', token=admin_token, json=ascent_data)
     assert rv.status_code == 400
 
 
-def test_create_ascent_non_existing_line(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_create_ascent_non_existing_line(client, member_token):
     ascent_data = {
         "flash": True,
         "fa": False,
@@ -146,16 +141,18 @@ def test_create_ascent_non_existing_line(client):
         "date": "2024-04-13"
     }
 
-    rv = client.post('/api/ascents', headers=access_headers, json=ascent_data)
+    rv = client.post('/api/ascents', token=member_token, json=ascent_data)
     assert rv.status_code == 404
 
 
 def test_successful_get_ascents(client):
+    ascent = Ascent.query.first()
+
     rv = client.get('/api/ascents?page=1')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res['items']) == 1
-    assert res['items'][0]['id'] == "55e840b9-2036-4725-a408-67d977adece5"
+    assert res['items'][0]['id'] == str(ascent.id)
     assert res['items'][0]['flash'] == False
     assert res['items'][0]['fa'] == True
     assert res['items'][0]['soft'] == False
@@ -173,14 +170,14 @@ def test_successful_get_ascents(client):
     assert res['items'][0]['crag']['slug'] == "brione"
     assert res['hasNext'] == False
 
-    rv = client.get('/api/ascents?crag_id=6b9e873b-e48d-4f0e-9d86-c3b6d7aa9db0&page=1')
+    rv = client.get(f'/api/ascents?crag_id={Crag.find_by_slug("chironico").id}&page=1')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res['items']) == 0
 
-    rv = client.get('/api/ascents?crag_id=aabc4539-c02f-4a03-8db3-ea0916e59884&page=1')
+    rv = client.get(f'/api/ascents?crag_id={Crag.find_by_slug("brione").id}&page=1')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res['items']) == 1
 
     rv = client.get('/api/ascents?page=2')
@@ -188,24 +185,26 @@ def test_successful_get_ascents(client):
 
 
 def test_successful_get_ticks(client):
+    admin = User.find_by_email("admin@localcrag.invalid.org")
+    member = User.find_by_email("member@localcrag.invalid.org")
+    ascent = Ascent.query.first()
     rv = client.get(
-        '/api/ticks?user_id=1543885f-e9ef-48c5-a396-6c898fb42409&crag_id=aabc4539-c02f-4a03-8db3-ea0916e59884')
+        f'/api/ticks?user_id={admin.id}&crag_id={Crag.find_by_slug("brione").id}')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res) == 1
-    assert res[0] == "1c39fd1f-6341-4161-a83f-e5de0f861c48"
+    assert res[0] == str(ascent.line_id)
 
     rv = client.get(
-        '/api/ticks?user_id=2543885f-e9ef-48c5-a396-6c898fb42409&crag_id=aabc4539-c02f-4a03-8db3-ea0916e59884')
+        f'/api/ticks?user_id={member.id}&crag_id={Crag.find_by_slug("brione").id}')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res) == 0
 
 
-def test_successful_delete_ascent(client):
-    access_headers, refresh_headers = get_login_headers(client)
-
-    rv = client.delete('/api/ascents/55e840b9-2036-4725-a408-67d977adece5', headers=access_headers)
+def test_successful_delete_ascent(client, admin_token):
+    ascent = Ascent.query.first()
+    rv = client.delete(f'/api/ascents/{ascent.id}', token=admin_token)
     assert rv.status_code == 204
 
     # Line must be still there
@@ -214,36 +213,20 @@ def test_successful_delete_ascent(client):
 
 
 def test_delete_user_deletes_tick_but_not_line(client):
-    # Change ascent owner as you cannot delete own user
-    with app.app_context():
-        with db.engine.begin() as conn:
-            conn.execute(text(
-                "update ascents set created_by_id = '2543885f-e9ef-48c5-a396-6c898fb42409' where created_by_id = '1543885f-e9ef-48c5-a396-6c898fb42409';"))
-
-    # Make user a non-superadmin as superadmins cannot be deleted
-    with app.app_context():
-        with db.engine.begin() as conn:
-            conn.execute(text(
-                "update users set superadmin = false where id = '2543885f-e9ef-48c5-a396-6c898fb42409';"))
-
-    access_headers, refresh_headers = get_login_headers(client)
-
-    rv = client.delete('/api/users/2543885f-e9ef-48c5-a396-6c898fb42409', headers=access_headers, json=None)
-    assert rv.status_code == 204
+    admin = User.find_by_email("admin@localcrag.invalid.org")
+    db.session.delete(admin)
 
     # Tick is gone
     rv = client.get('/api/ascents?page=1')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res['items']) == 0
 
     # Line must be still there
     rv = client.get('/api/lines/super-spreader')
     assert rv.status_code == 200
 
-def test_send_project_climbed_message(client, mocker):
-    access_headers, refresh_headers = get_login_headers(client)
-
+def test_send_project_climbed_message(client, mocker, moderator_token, user_token):
     # Create a project line first
     line_data = {
         "name": "Es",
@@ -291,9 +274,9 @@ def test_send_project_climbed_message(client, mocker):
         "secret": False,
     }
 
-    rv = client.post('/api/areas/dritter-block-von-links/lines', headers=access_headers, json=line_data)
+    rv = client.post('/api/areas/dritter-block-von-links/lines', token=moderator_token, json=line_data)
     assert rv.status_code == 201
-    res = json.loads(rv.data)
+    res = rv.json
     line_id = res['id']
 
     mock_SMTP_SSL = mocker.MagicMock(name="util.email.smtplib.SMTP_SSL")
@@ -302,7 +285,7 @@ def test_send_project_climbed_message(client, mocker):
         "line": line_id,
         "message": "I climbed the project! I think it's a 9A+ boulder. Cheers, Aidan Roberts"
     }
-    rv = client.post('/api/ascents/send-project-climbed-message', headers=access_headers, json=project_climbed_data)
+    rv = client.post('/api/ascents/send-project-climbed-message', token=user_token, json=project_climbed_data)
     assert rv.status_code == 204
 
     # We have three admins, so the mail must be sent three times
@@ -310,12 +293,14 @@ def test_send_project_climbed_message(client, mocker):
     assert mock_SMTP_SSL.return_value.__enter__.return_value.sendmail.call_count == 3
     assert mock_SMTP_SSL.return_value.__enter__.return_value.quit.call_count == 3
 
+    treppe = str(Line.get_id_by_slug("treppe"))
+
     # Try sending a project climbed message for a line that is not a project
     project_climbed_data = {
-        "line": "1c39fd1f-6341-4161-a83f-e5de0f861c48",
+        "line": treppe,
         "message": "I climbed the project! I think it's a 9A+ boulder. Cheers, Aidan Roberts"
     }
-    rv = client.post('/api/ascents/send-project-climbed-message', headers=access_headers, json=project_climbed_data)
+    rv = client.post('/api/ascents/send-project-climbed-message', token=user_token, json=project_climbed_data)
     assert rv.status_code == 400
 
     # Try for a non existing line
@@ -324,6 +309,6 @@ def test_send_project_climbed_message(client, mocker):
         "message": "I climbed the project! I think it's a 9A+ boulder. Cheers, Aidan Roberts"
     }
 
-    rv = client.post('/api/ascents/send-project-climbed-message', headers=access_headers, json=project_climbed_data)
+    rv = client.post('/api/ascents/send-project-climbed-message', token=user_token, json=project_climbed_data)
     assert rv.status_code == 404
 
