4bd44461186809cdca7593f97318dfbda6445360
Ben Swierzy <swierzy@uni-bonn.de>
2024-10-13 10:19:36 +0200

    Migrated test_line_path_resources

--- server/tests/test_line_path_resources.py
+++ server/tests/test_line_path_resources.py
@@ -1,121 +1,122 @@
-import json
+from models.area import Area
+from models.line import Line
+from models.line_path import LinePath
+from models.topo_image import TopoImage
 
-from tests.utils.user_test_util import get_login_headers
 
-
-def test_successful_delete_line_path(client):
-    access_headers, refresh_headers = get_login_headers(client)
-
-    rv = client.delete('/api/line-paths/74b746bb-2d79-4ba5-800d-a73af2c60507', headers=access_headers)
+def test_successful_delete_line_path(client, moderator_token):
+    line_path = LinePath.query.first()
+    rv = client.delete(f'/api/line-paths/{line_path.id}', token=moderator_token)
     assert rv.status_code == 204
 
 
-def test_successful_add_line_path(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_successful_add_line_path(client, moderator_token):
+    treppe = Line.find_by_slug("treppe")
+    topo_image = TopoImage.query.filter_by(area_id=treppe.area_id).order_by(TopoImage.order_index.desc()).first()
     line_path_data = {
-        "line": "9d64b102-95cd-4123-a2d1-4bb1f7c77ba0",
+        "line": str(treppe.id),
         "path": [1, 2, 3, 4]
     }
-    rv = client.post('/api/topo-images/f4625acb-b0fe-41f6-ab3c-fa258e586f2c/line-paths', headers=access_headers,
-                     json=line_path_data)
-    assert rv.status_code == 201
-    res = json.loads(rv.data)
+    rv = client.post(f'/api/topo-images/{topo_image.id}/line-paths', token=moderator_token, json=line_path_data)
+    assert rv.status_code == 201, rv.text
+    res = rv.json
     assert type(res['id']) == str
-    assert res['line']['id'] == "9d64b102-95cd-4123-a2d1-4bb1f7c77ba0"
+    assert res['line']['id'] == str(treppe.id)
     assert len(res['path']) == 4
 
 
-def test_add_line_path_path_too_short(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_add_line_path_path_too_short(client, moderator_token):
+    treppe = Line.find_by_slug("treppe")
+    topo_image = TopoImage.query.filter_by(area_id=treppe.area_id).order_by(TopoImage.order_index.desc()).first()
     line_path_data = {
-        "line": "9d64b102-95cd-4123-a2d1-4bb1f7c77ba0",
+        "line": str(treppe.id),
         "path": [1, 2]
     }
-    rv = client.post('/api/topo-images/f4625acb-b0fe-41f6-ab3c-fa258e586f2c/line-paths', headers=access_headers,
-                     json=line_path_data)
+    rv = client.post(f'/api/topo-images/{topo_image.id}/line-paths', token=moderator_token, json=line_path_data)
     assert rv.status_code == 400
 
 
-def test_add_line_path_path_out_of_bounds(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_add_line_path_path_out_of_bounds(client, moderator_token):
+    treppe = Line.find_by_slug("treppe")
+    topo_image = TopoImage.query.filter_by(area_id=treppe.area_id).order_by(TopoImage.order_index.desc()).first()
     line_path_data = {
-        "line": "9d64b102-95cd-4123-a2d1-4bb1f7c77ba0",
+        "line": str(treppe.id),
         "path": [1, 2, 101, 101]
     }
-    rv = client.post('/api/topo-images/f4625acb-b0fe-41f6-ab3c-fa258e586f2c/line-paths', headers=access_headers,
-                     json=line_path_data)
+    rv = client.post(f'/api/topo-images/{topo_image.id}/line-paths', token=moderator_token, json=line_path_data)
     assert rv.status_code == 400
 
 
-def test_add_line_path_path_not_even(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_add_line_path_path_not_even(client, moderator_token):
+    treppe = Line.find_by_slug("treppe")
+    topo_image = TopoImage.query.filter_by(area_id=treppe.area_id).order_by(TopoImage.order_index.desc()).first()
     line_path_data = {
-        "line": "9d64b102-95cd-4123-a2d1-4bb1f7c77ba0",
+        "line": str(treppe.id),
         "path": [1, 2, 100]
     }
-    rv = client.post('/api/topo-images/f4625acb-b0fe-41f6-ab3c-fa258e586f2c/line-paths', headers=access_headers,
-                     json=line_path_data)
+    rv = client.post(f'/api/topo-images/{topo_image.id}/line-paths', token=moderator_token, json=line_path_data)
     assert rv.status_code == 400
 
 
-def test_add_line_path_path_duplicate_line(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_add_line_path_path_duplicate_line(client, moderator_token):
+    treppe = Line.find_by_slug("treppe")
+    topo_image = TopoImage.query.filter_by(area_id=treppe.area_id).order_by(TopoImage.order_index).first()
     line_path_data = {
-        "line": "9d64b102-95cd-4123-a2d1-4bb1f7c77ba0",
+        "line": str(treppe.id),
         "path": [1, 2, 10, 100]
     }
-    rv = client.post('/api/topo-images/4e8f0a85-b971-409b-a972-7805173b4a19/line-paths', headers=access_headers,
-                     json=line_path_data)
+    rv = client.post(f'/api/topo-images/{topo_image.id}/line-paths', token=moderator_token, json=line_path_data)
     assert rv.status_code == 400
 
 
-def test_successful_order_line_paths(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_successful_order_line_paths(client, moderator_token):
+    line_paths = (LinePath.query.join(LinePath.line)
+                  .filter_by(area_id=Area.get_id_by_slug("dritter-block-von-links"))
+                  .order_by(LinePath.order_index).all())
 
     rv = client.get('/api/areas/dritter-block-von-links/topo-images')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res) == 2
-    assert res[0]['linePaths'][0]['id'] == "74b746bb-2d79-4ba5-800d-a73af2c60507"
-    assert res[0]['linePaths'][1]['id'] == "f2c62a33-373d-46ab-94ee-7da16239e126"
+    assert res[0]['linePaths'][0]['id'] == str(line_paths[0].id)
+    assert res[0]['linePaths'][1]['id'] == str(line_paths[1].id)
+    topo_image_id = res[0]['id']
 
     new_order = {
-        "74b746bb-2d79-4ba5-800d-a73af2c60507": 1,
-        "f2c62a33-373d-46ab-94ee-7da16239e126": 0,
+        str(line_paths[0].id): 1,
+        str(line_paths[1].id): 0,
     }
-    rv = client.put('/api/topo-images/4e8f0a85-b971-409b-a972-7805173b4a19/line-paths/update-order',
-                    headers=access_headers, json=new_order)
+    rv = client.put(f'/api/topo-images/{topo_image_id}/line-paths/update-order', token=moderator_token, json=new_order)
     assert rv.status_code == 200
 
     rv = client.get('/api/areas/dritter-block-von-links/topo-images')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res) == 2
-    assert res[0]['linePaths'][0]['id'] == "f2c62a33-373d-46ab-94ee-7da16239e126"
-    assert res[0]['linePaths'][1]['id'] == "74b746bb-2d79-4ba5-800d-a73af2c60507"
+    assert res[0]['linePaths'][0]['id'] == str(line_paths[1].id)
+    assert res[0]['linePaths'][1]['id'] == str(line_paths[0].id)
 
 
-def test_successful_order_line_paths_for_line(client):
-    access_headers, refresh_headers = get_login_headers(client)
+def test_successful_order_line_paths_for_line(client, moderator_token):
+    line_paths = LinePath.query.filter_by(line_id=Line.get_id_by_slug("super-spreader")).order_by(LinePath.order_index).all()
 
     rv = client.get('/api/lines/super-spreader')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res['linePaths']) == 2
-    assert res['linePaths'][0]['id'] == "74b746bb-2d79-4ba5-800d-a73af2c60507"
-    assert res['linePaths'][1]['id'] == "16781c0a-d816-4d0d-b9e5-4b23218f8b18"
+    assert res['linePaths'][0]['id'] == str(line_paths[0].id)
+    assert res['linePaths'][1]['id'] == str(line_paths[1].id)
 
     new_order = {
-        "16781c0a-d816-4d0d-b9e5-4b23218f8b18": 0,
-        "74b746bb-2d79-4ba5-800d-a73af2c60507": 1,
+        str(line_paths[0].id): 1,
+        str(line_paths[1].id): 0,
     }
-    rv = client.put('/api/lines/super-spreader/line-paths/update-order',
-                    headers=access_headers, json=new_order)
+    rv = client.put('/api/lines/super-spreader/line-paths/update-order', token=moderator_token, json=new_order)
     assert rv.status_code == 200
 
     rv = client.get('/api/lines/super-spreader')
     assert rv.status_code == 200
-    res = json.loads(rv.data)
+    res = rv.json
     assert len(res['linePaths']) == 2
-    assert res['linePaths'][1]['id'] == "74b746bb-2d79-4ba5-800d-a73af2c60507"
-    assert res['linePaths'][0]['id'] == "16781c0a-d816-4d0d-b9e5-4b23218f8b18"
+    assert res['linePaths'][0]['id'] == str(line_paths[1].id)
+    assert res['linePaths'][1]['id'] == str(line_paths[0].id)
