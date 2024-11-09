c78f0ebb22a359e7e4656b05e8c288cc72ace59b
Felix Engelmann <felix.engelmann@labnode.eu>
2024-11-09 21:24:19 +0100

    Move line gallery after line ascends

--- client/src/app/modules/line/line/line.component.ts
+++ client/src/app/modules/line/line/line.component.ts
@@ -107,14 +107,14 @@ export class LineComponent implements OnInit {
             routerLinkActiveOptions: { exact: true },
           },
           {
-            label: this.translocoService.translate(marker('line.gallery')),
-            icon: 'pi pi-fw pi-images',
-            routerLink: `/topo/${this.crag.slug}/${this.sector.slug}/${this.area.slug}/${this.line.slug}/gallery`,
-          },
-          {
             label: this.translocoService.translate(marker('line.ascents')),
             icon: 'pi pi-fw pi-check-square',
             routerLink: `/topo/${this.crag.slug}/${this.sector.slug}/${this.area.slug}/${this.line.slug}/ascents`,
+          },
+          {
+            label: this.translocoService.translate(marker('line.gallery')),
+            icon: 'pi pi-fw pi-images',
+            routerLink: `/topo/${this.crag.slug}/${this.sector.slug}/${this.area.slug}/${this.line.slug}/gallery`,
           },
           {
             label: this.translocoService.translate(marker('line.edit')),
