7ea4870d22162f6cad8437d587a54b545f8d6076
Felix Engelmann <felix.engelmann@labnode.eu>
2024-11-12 07:33:55 +0100

    Linting

--- client/src/app/modules/shared/components/tag/tag.component.ts
+++ client/src/app/modules/shared/components/tag/tag.component.ts
@@ -1,42 +1,42 @@
 import {
-    Component,
-    Input,
-    OnChanges,
-    SimpleChanges,
-    ViewEncapsulation,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewEncapsulation,
 } from '@angular/core';
-import {Tag} from '../../../../models/tag';
-import {TagModule} from 'primeng/tag';
-import {NgIf} from '@angular/common';
-import {AvatarModule} from 'primeng/avatar';
-import {RouterLink} from '@angular/router';
-import {EntityLinkPipe} from '../../pipes/entity-link.pipe';
-import {SearchableComponent} from '../../../core/searchable/searchable.component';
-import {Searchable} from '../../../../models/searchable';
+import { Tag } from '../../../../models/tag';
+import { TagModule } from 'primeng/tag';
+import { NgIf } from '@angular/common';
+import { AvatarModule } from 'primeng/avatar';
+import { RouterLink } from '@angular/router';
+import { EntityLinkPipe } from '../../pipes/entity-link.pipe';
+import { SearchableComponent } from '../../../core/searchable/searchable.component';
+import { Searchable } from '../../../../models/searchable';
 
 @Component({
-    selector: 'lc-tag',
-    standalone: true,
-    imports: [
-        TagModule,
-        NgIf,
-        AvatarModule,
-        EntityLinkPipe,
-        RouterLink,
-        SearchableComponent,
-    ],
-    templateUrl: './tag.component.html',
-    styleUrl: './tag.component.scss',
-    encapsulation: ViewEncapsulation.None,
+  selector: 'lc-tag',
+  standalone: true,
+  imports: [
+    TagModule,
+    NgIf,
+    AvatarModule,
+    EntityLinkPipe,
+    RouterLink,
+    SearchableComponent,
+  ],
+  templateUrl: './tag.component.html',
+  styleUrl: './tag.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class TagComponent implements OnChanges {
-    @Input() tag: Tag;
+  @Input() tag: Tag;
 
-    public searchable: Searchable;
+  public searchable: Searchable;
 
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes.tag) {
-            this.searchable = Searchable.fromObject(this.tag.object);
-        }
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.tag) {
+      this.searchable = Searchable.fromObject(this.tag.object);
     }
+  }
 }
