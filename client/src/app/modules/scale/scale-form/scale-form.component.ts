5884cebc89eda77f94171ed876420cf479dd182b
Ben Swierzy <swierzy@uni-bonn.de>
2024-12-08 20:43:12 +0100

    Scale form: Improved design and enhanced functionality

--- client/src/app/modules/scale/scale-form/scale-form.component.ts
+++ client/src/app/modules/scale/scale-form/scale-form.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { UntilDestroy } from '@ngneat/until-destroy';
 import { ScalesService } from '../../../services/crud/scales.service';
-import { TranslocoDirective } from '@jsverse/transloco';
+import { TranslocoDirective, TranslocoPipe } from '@jsverse/transloco';
 import { CardModule } from 'primeng/card';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormDirective } from '../../shared/forms/form.directive';
 import { LoadingState } from '../../../enums/loading-state';
 import { Scale } from '../../../models/scale';
@@ -11,8 +11,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { LineType } from '../../../enums/line-type';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'lc-scale-form',
@@ -24,7 +28,14 @@ import { SharedModule } from '../../shared/shared.module';
     CardModule,
     ReactiveFormsModule,
     NgForOf,
-    SharedModule
+    SharedModule,
+    TranslocoPipe,
+    NgIf,
+    InputTextModule,
+    InputNumberModule,
+    ToolbarModule,
+    ButtonModule,
+    FormsModule
   ]
 })
 @UntilDestroy()
@@ -78,4 +89,35 @@ export class ScaleFormComponent implements OnInit {
     return this.scaleForm.controls.grades as FormArray;
   }
 
+  reorderByValue() {
+    this.scaleForm.disable();
+    const data = this.gradeControls().value;
+    data.sort((a, b) => a.value - b.value);
+    this.gradeControls().clear();
+    data.filter((g) => Number.isInteger(g.value)).map((g) => this.fb.group({
+      name: [g.name],
+      value: [g.value],
+    })).forEach((ctl) => this.gradeControls().push(ctl));
+    this.scaleForm.enable();
+  }
+
+  addGrade() {
+    this.gradeControls().push(this.fb.group({name: [], value: [42]}));
+  }
+
+  deleteValue(index: number) {
+    this.scaleForm.disable();
+    this.gradeControls().removeAt(index);
+    this.scaleForm.enable();
+  }
+
+  saveScale() {
+    // todo
+  }
+
+  deleteScale() {
+    // todo
+  }
+
+  protected readonly LoadingState = LoadingState;
 }
