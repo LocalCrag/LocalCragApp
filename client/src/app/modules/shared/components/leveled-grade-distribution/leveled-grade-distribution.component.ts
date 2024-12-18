c0aede4fb84820a9b333f6218aeb50f064e5e33e
Ben Swierzy <swierzy@uni-bonn.de>
2024-12-18 18:30:09 +0100

    Fixed leveledGradeDistribution

--- client/src/app/modules/shared/components/leveled-grade-distribution/leveled-grade-distribution.component.ts
+++ client/src/app/modules/shared/components/leveled-grade-distribution/leveled-grade-distribution.component.ts
@@ -5,15 +5,14 @@ import { LineType } from '../../../../enums/line-type';
 import { ScalesService } from '../../../../services/crud/scales.service';
 import { TranslocoService } from '@jsverse/transloco';
 import { marker } from '@jsverse/transloco-keys-manager/marker';
+import { map } from 'rxjs/operators';
 
 type StackChartData = {
   lineType: LineType;
   gradeScale: string;
-  data: {
-    projects: number;
-    brackets: number[];
-    bracketLabels: string[];
-  };
+  projects: number;
+  brackets: number[];
+  bracketLabels: string[];
   total: number;
 }
 
@@ -28,7 +27,7 @@ type StackChartData = {
 export class LeveledGradeDistributionComponent implements OnInit {
   @Input() fetchingObservable: Observable<GradeDistribution>;
 
-  public stackChartData = [];
+  public stackChartData = null;
   public gradeDistribution: GradeDistribution;
   public gradeDistributionEmpty = true;
 
@@ -51,59 +50,61 @@ export class LeveledGradeDistributionComponent implements OnInit {
     const stackChartData: StackChartData[] = [];
     let gradeDistributionEmpty = true;
 
+    const observables: Observable<any>[] = [];
+
     for (const lineType in this.gradeDistribution) {
       for (const gradeScale in this.gradeDistribution[lineType]) {
-        forkJoin([
+        observables.push(forkJoin([
           this.scalesService.getScale(lineType as LineType, gradeScale),
           this.scalesService.gradeNameByValueMap(lineType as LineType, gradeScale)
-        ]).subscribe(([scale, gradeNameByValueMap]) => {
-            const labels = Array(scale.gradeBrackets.length).fill("");
+        ]).pipe(map(([scale, gradeNameByValueMap]) => {
+          const labels = Array(scale.gradeBrackets.length).fill("");
 
-            for (let i = 0; i < scale.gradeBrackets.length; i++) {
-              if (i == 0) {
-                labels.push(`${this.translocoService.translate(marker("leveledGradeDistributionUntil"))} ${gradeNameByValueMap[scale.gradeBrackets[i]]}`);
-              } else if (i == scale.gradeBrackets.length - 1) {
-                labels.push(`${this.translocoService.translate(marker("leveledGradeDistributionFrom"))} ${gradeNameByValueMap[scale.gradeBrackets[i]]}`);
-              } else {
-                labels.push(`${gradeNameByValueMap[scale.gradeBrackets[i-1] + 1]} - ${gradeNameByValueMap[scale.gradeBrackets[i]]}`); // todo here we assume that grade values are spaced by 1
-              }
+          for (let i = 0; i < scale.gradeBrackets.length; i++) {
+            if (i == 0) {
+              labels[i] = `${this.translocoService.translate(marker("leveledGradeDistributionUntil"))} ${gradeNameByValueMap[scale.gradeBrackets[i]]}`;
+            } else if (i == scale.gradeBrackets.length - 1) {
+              labels[i] = `${this.translocoService.translate(marker("leveledGradeDistributionFrom"))} ${gradeNameByValueMap[scale.gradeBrackets[i]]}`;
+            } else {
+              labels[i] = `${gradeNameByValueMap[scale.gradeBrackets[i-1] + 1]} - ${gradeNameByValueMap[scale.gradeBrackets[i]]}`; // todo here we assume that grade values are spaced by 1
             }
+          }
 
-            const data: StackChartData = {
-              lineType: lineType as LineType,
-              gradeScale,
-              data: {
-                projects: 0,
-                brackets: Array(scale.gradeBrackets.length).fill(0),
-                bracketLabels: labels,
-              },
-              total: 0,
-            }
-            for (const gradeValue of Object.keys(this.gradeDistribution[lineType][gradeScale]).map(Number)) {
-              gradeDistributionEmpty = false;
+          const data: StackChartData = {
+            lineType: lineType as LineType,
+            gradeScale,
+            projects: 0,
+            brackets: Array(scale.gradeBrackets.length).fill(0),
+            bracketLabels: labels,
+            total: 0,
+          }
+          for (const gradeValue of Object.keys(this.gradeDistribution[lineType][gradeScale]).map(Number)) {
+            gradeDistributionEmpty = false;
 
-              const count = this.gradeDistribution[lineType][gradeScale][gradeValue];
-              data.total += count;
-              if (gradeValue <= 0) {
-                data.data.projects += count;
-              } else {
-                for (let i = 0; i < scale.gradeBrackets.length; i++) {
-                  const bracket = scale.gradeBrackets[i];
-                  if (i == scale.gradeBrackets.length - 1) {
-                    data.data.brackets[i] += count;
-                  } else if (gradeValue <= bracket) {
-                    data.data.brackets[i] += count;
-                    break;
-                  }
+            const count = this.gradeDistribution[lineType][gradeScale][gradeValue];
+            data.total += count;
+            if (gradeValue <= 0) {
+              data.projects += count;
+            } else {
+              for (let i = 0; i < scale.gradeBrackets.length; i++) {
+                const bracket = scale.gradeBrackets[i];
+                if (i == scale.gradeBrackets.length - 1) {
+                  data.brackets[i] += count;
+                } else if (gradeValue <= bracket) {
+                  data.brackets[i] += count;
+                  break;
                 }
               }
             }
-            stackChartData.push(data);
-        });
+          }
+          stackChartData.push(data);
+        })));
       }
     }
 
-    this.gradeDistributionEmpty = gradeDistributionEmpty;
-    this.stackChartData = stackChartData.sort((a, b) => a.total - b.total);
+    forkJoin(observables).subscribe(() => {
+      this.stackChartData = stackChartData.sort((a, b) => a.total - b.total);
+      this.gradeDistributionEmpty = gradeDistributionEmpty;
+    });
   }
 }
