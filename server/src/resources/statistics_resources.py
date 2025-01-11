e6adaf6e2a8aa2c65ddff53d085a5c69349ad9d0
Felix Engelmann <felix.engelmann@labnode.eu>
2025-01-11 13:47:34 +0100

    Add endpoint for crag completion

--- server/src/resources/statistics_resources.py
+++ server/src/resources/statistics_resources.py
@@ -33,14 +33,15 @@ class GetCompletion(MethodView):
             raise BadRequest("When filtering for grades, a min and max grade is required.")
 
         # Query for all lines and their associated crag, sector, and area
-        lines_query = (db.session.query(Area.id, Sector.id, Crag.id, Line.id)
-                 .select_from(Line)
-                 .join(Area, Line.area_id == Area.id)
-                 .join(Sector, Area.sector_id == Sector.id)
-                 .join(Crag, Sector.crag_id == Crag.id)
-                 .filter(Line.grade_value >= 0) # Don't include projects in progress
-                 .distinct(Line.id))
-
+        lines_query = (
+            db.session.query(Area.id, Sector.id, Crag.id, Line.id)
+            .select_from(Line)
+            .join(Area, Line.area_id == Area.id)
+            .join(Sector, Area.sector_id == Sector.id)
+            .join(Crag, Sector.crag_id == Crag.id)
+            .filter(Line.grade_value >= 0)  # Don't include projects in progress
+            .distinct(Line.id)
+        )
 
         if min_grade_value is not None and max_grade_value is not None:
             lines_query = lines_query.filter(Line.grade_value >= min_grade_value, Line.grade_value <= max_grade_value)
@@ -60,7 +61,8 @@ class GetCompletion(MethodView):
 
         # Query for all ascents and their associated crag, sector, and area for the user
         ascents_query = db.session.query(Ascent.area_id, Ascent.sector_id, Ascent.crag_id).filter(
-            Ascent.created_by_id == user_id)
+            Ascent.created_by_id == user_id
+        )
 
         # Filter secret spots
         if not get_show_secret():
@@ -78,9 +80,9 @@ class GetCompletion(MethodView):
             crag_id = str(line[2])  # Crag.id
             sector_id = str(line[1])  # Sector.id
             area_id = str(line[0])  # Area.id
-            crag_counts.setdefault(crag_id, {'total_lines': 0, 'ascents': 0})['total_lines'] += 1
-            sector_counts.setdefault(sector_id, {'total_lines': 0, 'ascents': 0})['total_lines'] += 1
-            area_counts.setdefault(area_id, {'total_lines': 0, 'ascents': 0})['total_lines'] += 1
+            crag_counts.setdefault(crag_id, {"total_lines": 0, "ascents": 0})["total_lines"] += 1
+            sector_counts.setdefault(sector_id, {"total_lines": 0, "ascents": 0})["total_lines"] += 1
+            area_counts.setdefault(area_id, {"total_lines": 0, "ascents": 0})["total_lines"] += 1
 
         # Count ascents for each crag, sector, and area
         for ascent in ascents:
@@ -88,17 +90,13 @@ class GetCompletion(MethodView):
             sector_id = str(ascent[1])  # Sector.id
             area_id = str(ascent[0])  # Area.id
             if crag_id in crag_counts:
-                crag_counts[crag_id]['ascents'] += 1
+                crag_counts[crag_id]["ascents"] += 1
             if sector_id in sector_counts:
-                sector_counts[sector_id]['ascents'] += 1
+                sector_counts[sector_id]["ascents"] += 1
             if area_id in area_counts:
-                area_counts[area_id]['ascents'] += 1
+                area_counts[area_id]["ascents"] += 1
 
         # Combine the results into a single data structure
-        result = {
-            'crags': crag_counts,
-            'sectors': sector_counts,
-            'areas': area_counts
-        }
+        result = {"crags": crag_counts, "sectors": sector_counts, "areas": area_counts}
 
         return result
