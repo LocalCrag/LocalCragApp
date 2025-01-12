ab37d8e0770dfd5974b6174542c798f61e079042
Felix Engelmann <felix.engelmann@labnode.eu>
2025-01-12 13:30:39 +0100

    v1.10.0 (#546)
    * Add endpoint for crag completion
    
    * Add endpoint for crag completion
    
    * Add completion frontend
    
    * Add completion frontend
    
    * Update i18n
    
    * Update i18n
    
    * Add tests
    
    * Add tests
    
    * Test new postgres setup action
    
    * Test new postgres setup action
    
    * Remove unnecessary postgres setup steps

---
+++ client/src/app/modules/shared/components/expand-button/expand-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'lc-expand-button',
+  standalone: true,
+  imports: [ButtonModule, NgIf],
+  templateUrl: './expand-button.component.html',
+  styleUrl: './expand-button.component.scss',
+})
+export class ExpandButtonComponent {
+  @Input() expanded: boolean = false;
+  @Output() expandedChange = new EventEmitter<boolean>();
+}
