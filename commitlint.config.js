aed4007037a1184aad73ab667b4989948daee27f
Felix Engelmann <felix.engelmann@labnode.eu>
2025-02-24 22:10:36 +0100

    Define allowed scopes in commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': [2, 'always', ['account', 'ascents', 'changelog', 'core', 'gallery', 'gym', 'maps', 'menu', 'menu-pages', 'news', 'ranking', 'scales', 'search', 'todos', 'topo', 'users']],
+    },
 };
