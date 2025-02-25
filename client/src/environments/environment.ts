6bb3aae442fbdbf6615d302c1a58453fa0384f98
Felix Engelmann <felix.engelmann@labnode.eu>
2025-02-25 07:18:47 +0100

    ci: add app versions in package.json and footer (#623)

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,9 @@
+import packageInfo from '../../package.json';
+
 export const environment = {
   production: false,
   apiHost: 'http://127.0.0.1:5000',
   language: 'de',
   skippedSlug: '_default',
+  version: packageInfo.version,
 };
