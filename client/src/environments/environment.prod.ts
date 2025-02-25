c429237cc39f55bcadba200233d4db83d871ef59
Felix Engelmann <felix.engelmann@labnode.eu>
2025-02-25 07:13:52 +0100

    Add versions in package.json and use version in app footer

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
+import packageInfo from '../../package.json';
+
 export const environment = {
   production: true,
   apiHost: '',
   language: 'de',
   skippedSlug: '_default',
+  version: packageInfo.version,
 };
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'scope-enum': [2, 'always', ['account', 'ascents', 'changelog', 'core', 'gallery', 'gym', 'maps', 'menu', 'menu-pages', 'news', 'ranking', 'scales', 'search', 'todos', 'topo', 'users']],
-    },
+    extends: ['@commitlint/config-conventional']
 };
