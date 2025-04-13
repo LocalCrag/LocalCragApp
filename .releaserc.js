25cb1c1a20859232e7e80713cea6bfd33c8a4dc4
Felix Engelmann <felix.engelmann@labnode.eu>
2025-04-13 13:40:27 +0200

    Revert releaserc changes

--- .releaserc.js
+++ .releaserc.js
@@ -1,59 +1,44 @@
 module.exports = {
-  branches: [
-    { name: "main" },
-    { name: "next", prerelease: true }
-  ],
-  tagFormat: "v${version}",
-  plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        releaseRules: [
-          { type: "style", release: "patch" }
-        ]
-      }
+    branches: [
+        {name: "main"},
+        {name: "next", prerelease: true}
     ],
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        writerOpts: {
-          transform: (commit, context) => {
-            if (commit.type === "style") {
-              return {
-                ...commit,
-                type: "Style Changes"
-              };
+    tagFormat: "v${version}",
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                releaseRules: [
+                    {type: "style", release: "patch"}
+                ]
             }
-            return commit;
-          }
-        }
-      }
-    ],
-    [
-      "@semantic-release/npm",
-      { npmPublish: false, pkgRoot: "." }
-    ],
-    [
-      "@semantic-release/npm",
-      { npmPublish: false, pkgRoot: "client" }
-    ],
-    [
-      "@semantic-release/npm",
-      { npmPublish: false, pkgRoot: "server" }
-    ],
-    "@semantic-release/changelog",
-    "@semantic-release/github",
-    [
-      "@semantic-release/git",
-      {
-        assets: [
-          "package.json",
-          "client/package.json",
-          "server/package.json",
-          "CHANGELOG.md"
         ],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/npm",
+            {npmPublish: false, pkgRoot: "."}
+        ],
+        [
+            "@semantic-release/npm",
+            {npmPublish: false, pkgRoot: "client"}
+        ],
+        [
+            "@semantic-release/npm",
+            {npmPublish: false, pkgRoot: "server"}
+        ],
+        "@semantic-release/changelog",
+        "@semantic-release/github",
+        [
+            "@semantic-release/git",
+            {
+                assets: [
+                    "package.json",
+                    "client/package.json",
+                    "server/package.json",
+                    "CHANGELOG.md"
+                ],
+                message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ]
     ]
-  ]
 };
