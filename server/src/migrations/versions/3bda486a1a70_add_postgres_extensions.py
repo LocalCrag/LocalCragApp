888b4b78a539257301d0cbab370a670a719db4d9
Felix Engelmann <felix.engelmann@labnode.eu>
2025-07-06 18:05:12 +0200

    Fix handling of postgres extensions

---
+++ server/src/migrations/versions/3bda486a1a70_add_postgres_extensions.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: 3bda486a1a70
+Revises: 4306dc5f0c18
+Create Date: 2025-07-06 17:59:08.195747
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "3bda486a1a70"
+down_revision = "4306dc5f0c18"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;")
+    op.execute("CREATE EXTENSION IF NOT EXISTS uuid-ossp;")
+
+
+def downgrade():
+    op.execute("DROP EXTENSION IF EXISTS fuzzystrmatch;")
+    op.execute("DROP EXTENSION IF EXISTS uuid-ossp;")
