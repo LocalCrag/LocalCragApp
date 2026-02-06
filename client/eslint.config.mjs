b15febef277b53e64e7654f4a2068191492125d5
Felix Engelmann <felix.engelmann@labnode.eu>
2026-02-06 12:22:22 +0100

    Linting

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import {defineConfig, globalIgnores} from "eslint/config";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
+import {fileURLToPath} from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import {FlatCompat} from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
