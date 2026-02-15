1307fe6569455664c7b63256308dc558fac944b7
Felix Engelmann <github@fengelmann.de>
2026-02-15 11:57:13 +0100

    test(cypress): add script for locally running cypress tests with dependencies (#959)

--- client/src/app/modules/topo-images/topo-image-details/topo-image-details.component.ts
+++ client/src/app/modules/topo-images/topo-image-details/topo-image-details.component.ts
@@ -2,18 +2,13 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TopoImage } from '../../../models/topo-image';
 
-import { TRANSLOCO_SCOPE, TranslocoDirective } from '@jsverse/transloco';
+import { TRANSLOCO_SCOPE } from '@jsverse/transloco';
 import { CoordinatesButtonComponent } from '../../shared/components/coordinates-button/coordinates-button.component';
 import { SanitizeHtmlPipe } from '../../shared/pipes/sanitize-html.pipe';
 
 @Component({
   selector: 'lc-topo-image-details',
-  imports: [
-    ButtonModule,
-    TranslocoDirective,
-    CoordinatesButtonComponent,
-    SanitizeHtmlPipe,
-  ],
+  imports: [ButtonModule, CoordinatesButtonComponent, SanitizeHtmlPipe],
   templateUrl: './topo-image-details.component.html',
   styleUrl: './topo-image-details.component.scss',
   encapsulation: ViewEncapsulation.None,
