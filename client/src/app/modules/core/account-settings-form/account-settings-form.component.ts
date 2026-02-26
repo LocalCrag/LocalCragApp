ab758cf1b633401d6feff7ae32af6d8886428aad
Felix Engelmann <felix.engelmann@labnode.eu>
2026-02-26 21:23:31 +0100

    Add localization for date pipe based on user language

--- client/src/app/modules/core/account-settings-form/account-settings-form.component.ts
+++ client/src/app/modules/core/account-settings-form/account-settings-form.component.ts
@@ -12,9 +12,6 @@ import { AccountSettings } from '../../../models/account-settings';
 import { toastNotification } from '../../../ngrx/actions/notifications.actions';
 import { Store } from '@ngrx/store';
 import { LanguageSelectComponent } from '../../shared/forms/controls/language-select/language-select.component';
-import { Tooltip } from 'primeng/tooltip';
-import { Divider } from 'primeng/divider';
-import { LanguageService } from '../../../services/core/language.service';
 
 @Component({
   selector: 'lc-account-settings-form',
@@ -27,8 +24,6 @@ import { LanguageService } from '../../../services/core/language.service';
     FormDirective,
     Button,
     LanguageSelectComponent,
-    Tooltip,
-    Divider,
   ],
   templateUrl: './account-settings-form.component.html',
   styleUrl: './account-settings-form.component.scss',
@@ -44,7 +39,6 @@ export class AccountSettingsFormComponent implements OnInit {
   private accountService = inject(AccountService);
   private fb = inject(FormBuilder);
   private store = inject(Store);
-  private languageService = inject(LanguageService);
 
   private buildForm() {
     this.accountSettingsForm = this.fb.group({
