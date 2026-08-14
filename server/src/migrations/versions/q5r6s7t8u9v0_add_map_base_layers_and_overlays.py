6dc96c93b08303d153271584d5499d3296538f43
Felix Engelmann <felix.engelmann@labnode.eu>
2026-08-14 22:07:35 +0200

    Fix migration issues

--- server/src/migrations/versions/q5r6s7t8u9v0_add_map_base_layers_and_overlays.py
+++ server/src/migrations/versions/q5r6s7t8u9v0_add_map_base_layers_and_overlays.py
@@ -1,8 +1,8 @@
-"""Add map_base_layers and map_overlays; migrate MapTiler key into style URLs
+"""Add map_base_layers, rename overlays, migrate MapTiler key into style URLs
 
-Revision ID: p4q5r6s7t8u9
-Revises: o3p4q5r6s7t8
-Create Date: 2026-08-14 21:40:00.000000
+Revision ID: q5r6s7t8u9v0
+Revises: p4q5r6s7t8u9
+Create Date: 2026-08-14 22:10:00.000000
 
 """
 
@@ -14,8 +14,8 @@ from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.sql import text
 
 # revision identifiers, used by Alembic.
-revision = "p4q5r6s7t8u9"
-down_revision = "o3p4q5r6s7t8"
+revision = "q5r6s7t8u9v0"
+down_revision = "p4q5r6s7t8u9"
 branch_labels = None
 depends_on = None
 
@@ -31,13 +31,9 @@ def upgrade():
                 server_default=empty_json,
             )
         )
-        batch_op.add_column(
-            sa.Column(
-                "map_overlays",
-                JSON(),
-                nullable=False,
-                server_default=empty_json,
-            )
+        batch_op.alter_column(
+            "rock_explorer_map_layers",
+            new_column_name="map_overlays",
         )
 
     conn = op.get_bind()
@@ -76,5 +72,8 @@ def upgrade():
 def downgrade():
     with op.batch_alter_table("instance_settings", schema=None) as batch_op:
         batch_op.add_column(sa.Column("maptiler_api_key", sa.String(length=120), nullable=True))
+        batch_op.alter_column(
+            "map_overlays",
+            new_column_name="rock_explorer_map_layers",
+        )
         batch_op.drop_column("map_base_layers")
-        batch_op.drop_column("map_overlays")
